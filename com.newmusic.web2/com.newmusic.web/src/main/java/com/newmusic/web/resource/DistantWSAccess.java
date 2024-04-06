4c780692f51ac4281abf6c4bff9a128da140f7d1
coline11 <3097271568@qq.com>
2024-04-06 20:16:45 +0800

    Test message

--- com.newmusic.web2/com.newmusic.web/src/main/java/com/newmusic/web/resource/DistantWSAccess.java
+++ com.newmusic.web2/com.newmusic.web/src/main/java/com/newmusic/web/resource/DistantWSAccess.java
@@ -19,6 +19,7 @@ import org.xml.sax.InputSource;
 import com.newmusic.web.data.Artist;
 
 public class DistantWSAccess {
+	//test
 	
 	public static String searchByArtistAlias(Artist a) {
 		return searchByArtist(a.getAlias());
